6d27e68ae6a0ce72051c750f507b74b7dbda0c1f
Abdul Moiz Hussain <144014195+aMoiz7@users.noreply.github.com>
2024-06-29 02:09:59 +0530

    Update sendEmail.ts

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -27,7 +27,7 @@ export const sendEmail = async (formData: FormData) => {
   try {
     data = await resend.emails.send({
       from: "Contact Form <onboarding@resend.dev>",
-      to: "perpixel000@gmail.com",
+      to: senderEmail,
       subject: "Message from contact form",
       reply_to: senderEmail,
       react: React.createElement(ContactFormEmail, {
